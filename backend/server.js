3cd990b88a753b5704b16eb07cb94a0bf6becf3e
sivaganesh2727 <sivaganeshragam2004@gmail.com>
2025-02-23 00:48:21 +0530

    Fixed bcrypt issue

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const AuthRouter = require("./Routes/AuthRouter");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
